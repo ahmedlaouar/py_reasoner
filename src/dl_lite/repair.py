d34bda022a3fa682049905e9ed4961ce7945ed1b
ahmed.laouar <ahmed.laouar@univ-artois.fr>
2023-05-12 08:43:36 +0200

    Fixing tests.

--- src/dl_lite/repair.py
+++ src/dl_lite/repair.py
@@ -1,11 +1,30 @@
 from abox import ABox
 from assertion import assertion
-from axiom import Axiom
+from axiom import Axiom, Modifier
 from tbox import TBox
 
 def same_individuals(axiom: Axiom, assertion_1: assertion, assertion_2: assertion):
-    if assertion_1.is_role() == assertion_2.is_role() and assertion_1.get_individuals() == assertion_2.get_individuals():
-        return True
+    # Check if assertions match with axiom sides 
+    if (assertion_1.get_assertion_name() == axiom.get_left_side().get_name() and assertion_2.get_assertion_name() == axiom.get_right_side().get_name()): #or (assertion_2_name == axiom_left_name and assertion_1_name == axiom_right_name) :
+        # if both are concepts or both are roles compare individuals
+        if assertion_1.is_role() == assertion_2.is_role() and assertion_1.get_individuals() == assertion_2.get_individuals():
+            return True
+        # if assertion_1 is role and assertion_2 is concept
+        if assertion_1.is_role():
+            if Modifier.projection in axiom.get_left_side().get_modifiers() and Modifier.inversion in axiom.get_left_side().get_modifiers():
+                if assertion_1.get_individuals()[1] == assertion_2.get_individuals():
+                    return True
+            if Modifier.projection in axiom.get_left_side().get_modifiers:
+                if assertion_1.get_individuals()[0] == assertion_2.get_individuals():
+                    return True
+        # if assertion_1 is concept and assertion_2 is role
+        if assertion_2.is_role():
+            if Modifier.projection in axiom.get_right_side().get_modifiers() and Modifier.inversion in axiom.get_right_side().get_modifiers():
+                if assertion_1.get_individuals() == assertion_2.get_individuals()[1]:
+                    return True
+            if Modifier.projection in axiom.get_right_side().get_modifiers:
+                if assertion_1.get_individuals() == assertion_2.get_individuals()[0]:
+                    return True
 
 def conflict_set(tbox: TBox, abox : ABox) -> list():
         conflicts = []
@@ -15,18 +34,10 @@ def conflict_set(tbox: TBox, abox : ABox) -> list():
         # Browse all negative axioms
         for axiom in negative_axioms:
             # Browse all assertions
-            axiom_left_name = axiom.get_left_side().get_name()
-            axiom_right_name = axiom.get_right_side().get_name()
             for assertion_1 in assertions:
                 # Browse all assertions after the current assertion (to avoid duplicate verifications)
                 for assertion_2 in assertions[assertions.index(assertion_1):] :
-                    assertion_1_name = assertion_1.get_assertion_name()
-                    assertion_2_name = assertion_2.get_assertion_name()        
-                    # Check if (assertion_1 name in assertion_2 name) or (assertion_2 name in assertion_1 name)
-                    if ( assertion_1_name == axiom_left_name and assertion_2_name == axiom_right_name) or \
-                          (assertion_2_name == axiom_left_name and assertion_1_name == axiom_right_name) :
-                        # replace the following call for a function in assertion to compare the individuals 
-                        # if both are concepts or both are roles compare individuals
-                        if same_individuals(axiom, assertion_1, assertion_2):
-                            conflicts.append((axiom, assertion_1, assertion_2))
+                    # replace the following call for a function in assertion to compare the individuals 
+                    if same_individuals(axiom, assertion_1, assertion_2) or same_individuals(axiom, assertion_2, assertion_1):
+                        conflicts.append((axiom, assertion_1, assertion_2))
         return conflicts

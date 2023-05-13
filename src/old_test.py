9c07200ea105e64aeb85dda67b5a558833cd6f34
ahmed.laouar <ahmed.laouar@univ-artois.fr>
2023-05-13 11:02:30 +0200

    Adding some tests for the parser.

---
+++ src/old_test.py
@@ -0,0 +1,73 @@
+from dl_lite.axiom import Axiom, Modifier, Side
+from dl_lite.assertion import assertion
+from dl_lite.abox import ABox
+from dl_lite.tbox import TBox
+from dl_lite.repair import conflict_set
+#Concepts
+human = "Human"
+man = "Man"
+chicken = "Chicken"
+#Roles
+eats = "Eats"
+#Individuals
+mark = "Mark"
+bill = "Bill"
+ch1 = "ch1"
+ch2 = "ch2"
+#Tbox sides and Axioms
+side1 = Side(human)
+side1n = Side(human, [Modifier.negation])
+side2 = Side(man, [Modifier.negation])
+side3 = Side(eats, [Modifier.inversion])
+side4 = Side(chicken)
+side5 = Side(eats, [Modifier.projection])
+side6 = Side(man)
+side7 = Side(chicken, [Modifier.negation])
+side8 = Side(eats, [Modifier.projection,Modifier.inversion])
+#Axioms
+ax3 = Axiom(side6,side1)
+ax1 = Axiom(side4,side1n)
+ax4 = Axiom(side1,side7)
+ax5 = Axiom(side6,side7)
+ax2 = Axiom(side5,side7)
+ax6 = Axiom(side8,side2)
+
+# a tbox with all possible axioms to check conflicts 
+# test 1
+axioms1 = [ax3,ax4,ax6,ax2]
+#TBOX
+tbox1 = TBox(axioms1)
+print(tbox1)
+tbox1.negative_closure()
+#print(tbox1)
+# test 2
+#axioms2 = [ax1,ax3]
+#tbox2 = TBox(axioms2)
+#print(tbox2)
+#tbox2.negative_closure()
+#print(tbox2)
+
+#Assertions 
+ass1 = assertion(man,mark)
+ass2 = assertion(eats,mark,ch1)
+ass3 = assertion(man,bill)
+ass4 = assertion(chicken,mark)
+ass5 = assertion(eats,mark,bill)
+ass6 = assertion(eats,ch1,ch2)
+ass7 = assertion(chicken,ch1)
+ass8 = assertion(chicken,ch2)
+#assertions = [ass1, ass3, ass4, ass5, ass6, ass7, ass8]
+abox = ABox()
+abox .add_assertion(ass1)
+abox .add_assertion(ass3)
+abox .add_assertion(ass4)
+abox .add_assertion(ass5)
+abox .add_assertion(ass6)
+abox .add_assertion(ass7)
+abox .add_assertion(ass8)
+assertions_list = abox.get_assertions()
+for ass in assertions_list:
+    print(ass)
+conflict_set = conflict_set(tbox1,abox)
+for conf in conflict_set :
+    print("Axiom : ", conf[0], " | Conflict : (", conf[1], ", ", conf[2],")")
---
+++ src/test_exemple_papier.py
@@ -0,0 +1,77 @@
+from dl_lite.axiom import Axiom, Modifier, Side
+from dl_lite.assertion import assertion
+from dl_lite.abox import ABox
+from dl_lite.tbox import TBox
+from dl_lite.repair import check_assertion_in_cpi_repair
+# Individuals
+f78 = "F78"
+bob = "Bob"
+# Concepts
+reports = "Reports"
+manager = "Manager"
+sales = "Sales"
+staff = "Staff"
+# Roles
+sign = "Sign"
+edit = "Edit"
+# Tbox axioms
+side1 = Side(manager)
+side2 = Side(edit, [Modifier.negation, Modifier.projection])
+side3= Side(sales)
+side4 = Side(sign, [Modifier.negation, Modifier.projection])
+side5 = Side(staff)
+axiom1 = Axiom(side1,side2)
+axiom2 = Axiom(side3,side4)
+axiom3 = Axiom(side1,side5)
+axiom4= Axiom(side3,side5)
+# TBox
+axioms = [axiom1,axiom2,axiom3,axiom4]
+tbox = TBox(axioms)
+print(tbox)
+# Assertions
+assertion1 = assertion(reports,f78)
+assertion2 = assertion(manager,bob)
+assertion3 = assertion(sales,bob)
+assertion4 = assertion(sign,bob,f78)
+assertion5 = assertion(edit,bob,f78)
+# Abox
+abox = ABox()
+abox.add_assertion(assertion1)
+abox.add_assertion(assertion2)
+abox.add_assertion(assertion3)
+abox.add_assertion(assertion4)
+abox.add_assertion(assertion5)
+
+# Partial order
+abox.add_directed_edge(assertion1,assertion2)
+#print(abox.get_all_graph())
+abox.add_directed_edge(assertion1,assertion3)
+abox.add_directed_edge(assertion2,assertion4)
+abox.add_directed_edge(assertion3,assertion5)
+
+assertions_list = abox.get_assertions()
+for ass in assertions_list:
+    print(ass)
+preference_relation = abox.get_directed_edges()
+for edge1,edge2 in preference_relation:
+    print(edge1, " -> ",edge2)
+
+assertion6 = assertion(staff,bob)
+print(f"Checking if {assertion6} is in Cpirepair de abox:")
+check_assertion_in_cpi_repair(abox,tbox,assertion6)
+
+# Compute conflicts set 
+#tbox.negative_closure()
+
+#conflict_set = conflict_set(tbox,abox)
+#for conf in conflict_set :
+#    print("Axiom : ", conf[0], " | Conflict : (", conf[1], ", ", conf[2],")")
+
+#supports = compute_supports(assertion6, tbox.get_positive_axioms(),abox)
+#print(f"The supports of {assertion6} are:")
+#for support in supports:
+#    print(support)
+
+# Checking if Staff(Bob) is in Cpirepair de abox:
+#if check_all_dominance(abox, conflict_set, supports):
+#    print(f"{assertion6} is in the Cpi-repair of the abox")

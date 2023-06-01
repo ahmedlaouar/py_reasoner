cd76832b3035c4eaf308f59c3481fffcfb8e4477
ahmed.laouar <ahmedlaouar.al@gmail.com>
2023-06-01 14:43:05 +0200

    Removing previous pos representation methods.
    Former-commit-id: 2118d6365e464d3c1af244e4d6bb53b442d96166

--- src/repair/dominance.py
+++ src/repair/dominance.py
@@ -1,22 +1,3 @@
-# deprecated, old pos representation is replaced with adjacency matrix representation
-def check_is_preferred_weight(pos,weight_1,weight_2) -> bool:
-    check = False
-    if weight_2 in pos[weight_1]: return True
-    else: 
-        for successor in pos[weight_1]:
-            check = check_is_preferred_weight(pos,successor,weight_2)
-    return check
-
-# deprecated, old pos representation is replaced with adjacency matrix representation
-def is_strictly_preferred_pos(cursor, pos, w_assertion_1, w_assertion_2) -> bool:
-    # a test if assertion_1 is strictly preferred to assertion_2
-    weight_1 = w_assertion_1.get_assertion_weight()
-    weight_2 = w_assertion_2.get_assertion_weight()
-    # Check if the assertions have the same weight associated
-    if weight_1 == weight_2:
-        return False
-    return check_is_preferred_weight(pos,weight_1,weight_2)
-
 # new implementation using pos_mat as an adjacency matrix 
 def is_strictly_preferred(pos_mat, w_assertion_1, w_assertion_2) -> bool:
     # a test if assertion_1 is strictly preferred to assertion_2

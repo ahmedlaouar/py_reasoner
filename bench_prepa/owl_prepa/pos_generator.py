1dfc9e23e6ce23d16fa6dcffeb11605fbd0a15dc
ahmed.laouar <ahmed.laouar@univ-artois.fr>
2024-01-29 23:46:40 +0100

    Add partially order set pos to data.

---
+++ bench_prepa/owl_prepa/pos_generator.py
@@ -0,0 +1,31 @@
+import random
+
+def generate_random_dag(num_nodes):
+    graph = {}
+
+    for i in range(num_nodes):
+        graph[i] = []
+
+        for j in range(0, num_nodes):
+            if random.random() < 0.75 and j != i:
+                graph[i].append(j)
+
+    return graph
+
+def save_dag_to_file(graph, filename):
+    with open(filename, 'w') as file:
+        for element in graph:
+            to_write = [str(x) for x in graph[element]]
+            if len(to_write) == 0:
+                pos_line = str(element)+" \n"
+            else:
+                pos_line = str(element)+" "+" ".join(to_write)+" \n"
+            file.write(pos_line)
+
+if __name__ == '__main__':
+    # Generate a random DAG with "50", "500", "1000", "10000" nodes
+    num_nodes = 1000
+    dag = generate_random_dag(num_nodes)
+    # Save DAG to a text file
+    save_dag_to_file(dag, "bench_prepa/dataset.01/pos1000.txt")
+    print("DAG saved to bench_prepa/dataset.01/pos1000.txt")

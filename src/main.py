41b47a57008b0743aab0fdf757c1d8d5dce7c937
ahmed.laouar <ahmedlaouar.al@gmail.com>
2024-02-06 16:52:02 +0100

    Add POS generated with "ic-dag" method of bnlearn.

--- src/main.py
+++ src/main.py
@@ -41,9 +41,9 @@ def conflicts_helper(ontology_path,data_path) :
 if __name__ == "__main__":
     ontology_path = "ontologies/univ-bench/lubm-ex-20_disjoint.owl"
     
-    data_path = "bench_prepa/dataset.01/University0_p_0.001_pos_1000.db"
+    data_path = "bench_prepa/dataset.01/University0_p_0.001_pos_500.db"
     
-    pos_path = "bench_prepa/dataset.01/DAGs_with_bnlearn/melancon_method/pos1000_melancon.txt"
+    pos_path = "bench_prepa/dataset.01/DAGs_with_bnlearn/ordered_method/pos500_prob_0.75.txt"
 
     results1 = compute_pi_repair(ontology_path,data_path,pos_path)
 

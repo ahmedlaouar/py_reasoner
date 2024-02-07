a015eb6251e21a2085fffd29289c69479f847e70
ahmed.laouar <ahmedlaouar.al@gmail.com>
2024-02-07 18:53:07 +0100

    Tests with ordered method POS_1000.
    Former-commit-id: 20bbc0087c5985daef80586764745f0ba8f77e5f [formerly f07681b2758462edaddd4fd19350889e854cf6d3]
    Former-commit-id: 9bc8647deca9bfa9cb8a8c01c78a4a616790867a

--- src/repair/utils.py
+++ src/repair/utils.py
@@ -1,3 +1,6 @@
+import random
+import sqlite3
+
 def read_pos(file_path :str):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -8,3 +11,24 @@ def read_pos(file_path :str):
             successors = [int(x) for x in tokens[1:]]
             pos_dict[weight] = successors
     return pos_dict
+
+def add_pos_to_db(data_path:str, pos_path:str):
+    with open(pos_path, 'r') as file:
+        lines_number = sum(1 for line in file)
+    
+    conn = sqlite3.connect(data_path)
+    cursor = conn.cursor()
+
+    # Get the list of tables
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+
+    for table in tables:
+        cursor.execute(f"SELECT COUNT(*) FROM {table[0]}")
+        count = cursor.fetchone()[0]
+        for i in range(1,count+1):
+            degree = random.choice(range(lines_number))
+            cursor.execute(f"UPDATE {table[0]} SET degree = ? WHERE id = ?", (degree, i))
+    
+    conn.commit()
+    conn.close()

7581fd317f9bb98fc7dd48695d6ac15109b5e6cf
ahmed.laouar <ahmedlaouar.al@gmail.com>
2024-02-28 13:41:56 +0100

    More tests for only cpi plots.
    Former-commit-id: 747f2b270962552d1cfc68d5101d4be0250a05dc [formerly b4aa54d0cfa016602a20a80bb61926927a8f93b9]
    Former-commit-id: 01ab7447ef41b0cefd38fd58b3cda5f13f6af064

--- src/main2.py
+++ src/main2.py
@@ -47,19 +47,19 @@ if __name__ == "__main__":
     ontology_path = "ontologies/univ-bench/lubm-ex-20_disjoint.owl"
     
     # "bench_prepa/dataset.0.2/University5_p_0.00005.db", "bench_prepa/dataset.0.2/University5_p_0.0005.db", "bench_prepa/dataset.0.2/University5_p_0.0001.db", "bench_prepa/dataset.0.2/University5_p_0.00001.db", "bench_prepa/dataset.0.2/University5_p_0.000005.db"
-    # "bench_prepa/dataset_1_university/University0_p_0.000005.db", "bench_prepa/dataset_1_university/University0_p_0.00001.db", "bench_prepa/dataset_1_university/University0_p_0.00005.db", "bench_prepa/dataset_1_university/University0_p_0.00015.db", "bench_prepa/dataset_1_university/University0_p_0.0005.db"
-    data_paths = ["bench_prepa/dataset_small_u1/university0.5_p_0.00001.db"] #,"bench_prepa/dataset_small_u1/university0.5_p_0.000005.db"  
-    # , "bench_prepa/dataset_small_u1/university0.5_p_0.00005.db", "bench_prepa/dataset_small_u1/university0.5_p_0.0005.db", "bench_prepa/dataset_small_u1/university0.5_p_0.001.db"
+    # "bench_prepa/dataset_1_university/University0_p_0.000005.db", , "bench_prepa/dataset_1_university/University0_p_0.00005.db", "bench_prepa/dataset_1_university/University0_p_0.00015.db", "bench_prepa/dataset_1_university/University0_p_0.0005.db"
+    data_paths = ["bench_prepa/dataset_1_university/University0_p_0.00001.db"] #  
+    # "bench_prepa/dataset_small_u1/university0.5_p_0.000005.db", "bench_prepa/dataset_small_u1/university0.5_p_0.00001.db", "bench_prepa/dataset_small_u1/university0.5_p_0.00005.db", "bench_prepa/dataset_small_u1/university0.5_p_0.0005.db", "bench_prepa/dataset_small_u1/university0.5_p_0.001.db"
 
     #for data_path in data_paths:
     #    conflicts_helper(ontology_path, data_path)
     relative_path = "bench_prepa/DAGs/DAGs_with_bnlearn/ordered_method/"
     
-    pos_dir_paths = ["pos50/", "pos100/", "pos250/", "pos500/", "pos750/", "pos1000/"] #, "pos2500/"
+    pos_dir_paths = ["pos250/", "pos750/", "pos1000/", "pos2500/", "pos5000/"] # "pos50/", "pos100/", "pos500/", 
 
-    results_path = "bench_prepa/dataset.01/execution_results_smaller_u1_0.5.txt"
+    results_path = "bench_prepa/dataset.01/execution_results_pos_u1.txt"
 
-    pos_list = ["prob_0.1.txt", "prob_0.2.txt", "prob_0.3.txt", "prob_0.4.txt"] #, "prob_0.5.txt", "prob_0.6.txt", "prob_0.7.txt", "prob_0.8.txt", "prob_0.9.txt"
+    pos_list = ["prob_0.3.txt", "prob_0.4.txt"] #"prob_0.1.txt", "prob_0.2.txt", ,  "prob_0.5.txt", "prob_0.6.txt", "prob_0.7.txt", "prob_0.8.txt", "prob_0.9.txt"
 
     for data_path in data_paths:
             
@@ -81,7 +81,7 @@ if __name__ == "__main__":
                     ABox_name = data_path.split("/")[-1]
                     TBox_name = ontology_path.split("/")[-1]
 
-                    result = results1 + results2 + results3 #
+                    result = results1 + results3 # + results2
                     
                     result_str = str(ABox_name)+","+str(TBox_name)+","+str(pos_name)+","
                     

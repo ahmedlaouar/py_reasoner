5fc740ba03c404b24203920501ab11cf8b34403b
ahmed.laouar <ahmed.laouar@univ-artois.fr>
2024-04-07 21:58:47 +0200

    Add details on how to create and complete DAGs.
    Former-commit-id: 88c4e6fcef83ad30edeec7eb3268df80225acae3
    Former-commit-id: 7c7727b81e98811115b57cfd4e21fca0b0eb117e

--- bench_prepa/owl_prepa/pos_generator_bnlearn.r
+++ bench_prepa/owl_prepa/pos_generator_bnlearn.r
@@ -1,7 +1,6 @@
 save_dag_to_file <- function(graph, filename) {
   # Open the file for writing
   file <- file(filename, "w")
-
   # Loop through each node in the graph
   for (node in node_labels) {
     children_list <- random_dag$node[[node]]$children
@@ -11,13 +10,9 @@ save_dag_to_file <- function(graph, filename) {
     # Write the line to the file
     writeLines(paste(node, children_str), file)
   }
-
   # Close the file
   close(file)
 }
-
-# Load the Rgraphviz package
-#library(Rgraphviz)
 
 if(!requireNamespace("bnlearn", quietly = TRUE)) {
   install.packages("bnlearn")
@@ -25,7 +20,7 @@ if(!requireNamespace("bnlearn", quietly = TRUE)) {
 library(bnlearn)
 
 # Set the number of nodes in the graph
-num_nodes <- 5000 - 1
+num_nodes <- 100 - 1
 
 # Generate node labels
 node_labels <- paste0(0:num_nodes)
@@ -33,18 +28,13 @@ node_labels <- paste0(0:num_nodes)
 # Generate a random DAG
 # nodes: list of nodes. method (used algorithm): ordered (similar to randomDAG of pcalg python), ic-dag, melancon. prob: for ordered algo, probability of each arc burn.in: number of iterations for ic-dag and melancon.   
 probabilities <- seq(0.1, 1, by = 0.1)
-# random_dag <- random.graph(nodes = node_labels, method= "melancon")
-# random_dag <- random.graph(nodes = node_labels, method = "ic-dag", burn.in=6*500000)
 
 for (prob in probabilities) {
   random_dag <- random.graph(nodes = node_labels, method= "ordered", prob = prob)
-  
-  file_name = sprintf("bench_prepa/dataset.01/DAGs_with_bnlearn/ordered_method/pos5000/pos5000_prob_%.1f.txt", prob)
+  # random_dag <- random.graph(nodes = node_labels, method= "melancon")
+  # random_dag <- random.graph(nodes = node_labels, method = "ic-dag")
+
+  file_name = sprintf("bench_prepa/DAGs/DAGs_with_bnlearn/ordered_method/pos100/prob_%.1f.txt", prob)
   
   save_dag_to_file(random_dag, file_name)
 }
-
-#print(random_dag)
-
-# Plot the random DAG
-#graphviz.plot(random_dag)
